10848463ebc74334230d49abd3273213435a8f9a
VoydLogic <qrx9823@gmail.com>
2015-03-31 14:14:54 -0400

    Bubble Sort Algorithm

---
+++ BubbleSort.js
@@ -0,0 +1,27 @@
+function BubbleSort(array) {
+    for (var i = 0; i < array.length; ++i) {
+        for (var j = array.length - 1; j >= i; --j) {
+            // Least to greatest
+            if (array[j - 1] > array[j]) {
+                var temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+function BubbleSort(array) {
+    for (var i = 0; i < array.length; ++i) {
+        for (var j = array.length - 1; j >= i; --j) {
+            // Greatest to least
+            if (array[j - 1] < array[j]) {
+                var temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+            }
+        }
+    }
+    return array;
+}
