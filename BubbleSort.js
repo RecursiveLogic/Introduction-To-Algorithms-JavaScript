412319fbf4212d2566ddc2d106fa467f4c365527
RecursiveLogic <qrx9823@gmail.com>
2015-05-01 15:44:21 -0400

    Update

--- BubbleSort.js
+++ BubbleSort.js
@@ -3,9 +3,9 @@ function BubbleSort(array) {
         for (var j = array.length - 1; j >= i; --j) {
             // Least to greatest
             if (array[j - 1] > array[j]) {
-                var temp = array[j];
+                var key = array[j];
                 array[j] = array[j - 1];
-                array[j - 1] = temp;
+                array[j - 1] = key;
             }
         }
     }
@@ -17,9 +17,9 @@ function BubbleSort(array) {
         for (var j = array.length - 1; j >= i; --j) {
             // Greatest to least
             if (array[j - 1] < array[j]) {
-                var temp = array[j];
+                var key = array[j];
                 array[j] = array[j - 1];
-                array[j - 1] = temp;
+                array[j - 1] = key;
             }
         }
     }
